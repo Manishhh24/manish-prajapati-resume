821ea8f5f92535c043f2f0aef8a4c8dded57fffd
Manishhh24 <190420116054.it19@scet.ac.in>
2023-05-14 14:06:58 +0530

    tailwind.config.js modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily:{
+        libreBaskerVille: ['Libre Baskerville', 'sans']
+      },
+      screens:{
+        xxs:'343px'
+      }
     },
   },
   plugins: [],
