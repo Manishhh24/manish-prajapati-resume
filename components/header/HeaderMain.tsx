511cb972c964fb5be669548626624de3839b6082
Manishhh24 <190420116054.it19@scet.ac.in>
2023-05-14 11:04:37 +0530

    components folder created and file added

---
+++ components/header/HeaderMain.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderMain = () => {
+  return (
+    <>
+      <div>
+        <p className="text-red-500">Test message</p>
+      </div>
+    </>
+  );
+};
+
+export default HeaderMain;
