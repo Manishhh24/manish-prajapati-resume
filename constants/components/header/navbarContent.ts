55e90aaee8022bf52be2b4deb2546de93c4b24f6
Manishhh24 <190420116054.it19@scet.ac.in>
2023-05-20 20:41:53 +0530

    navbarContent constant file created

---
+++ constants/components/header/navbarContent.ts
@@ -0,0 +1,8 @@
+export const navbarContent = [
+    {label:"Home", link:""},
+    {label:"Education", link:""},
+    {label:"Projects", link:""},
+    {label:"MileStones", link:""},
+    {label:"Achievements", link:""},
+    {label:"Extra-curricular", link:""},
+];
