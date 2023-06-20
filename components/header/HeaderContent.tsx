22647c4cd136959221f16402910a13c04224d938
Manishhh24 <190420116054.it19@scet.ac.in>
2023-06-20 22:17:19 +0530

    anchor tag used in HeaderContent file

--- components/header/HeaderContent.tsx
+++ components/header/HeaderContent.tsx
@@ -4,14 +4,14 @@ import { AiOutlineMail, AiFillLinkedin } from "react-icons/ai";
 import { SiHackerrank } from "react-icons/si";
 
 const HeaderContent = () => {
-  return (
+  return (  
     <>
       <div className="flex justify-between py-5 bg-gradient-to-br from-blue-300 via-blue-400 to-blue-500 max-w-screen-2xl items-center">
         <h1 className="text-sm xxs:text-lg sm:text-2xl md:text-5xl text-white text-left pl-7 font-libreBaskerVille">
           MANISH &nbsp; PRAJAPATI
         </h1>
         <div className="pr-7 flex flex-row sm:flex-col gap-3 xs:gap-5 sm:gap-1">
-          <Link
+          <a
             href="mailto:mmprajapaty@gmail.com"
             className="text-white text-sm xxs:text-lg sm:text-sm xs:text-lg md:text-2xl flex flex-row items-center gap-2 hover:cursor-pointer hover:font-semibold transition-transform delay-700 duration-1000"
           >
@@ -19,7 +19,7 @@ const HeaderContent = () => {
             <p className="text-xs hidden sm:block md:text-sm">
               mmprajapaty@gmail.com
             </p>
-          </Link>
+          </a>
           <Link
             href="https://www.linkedin.com/in/manish-prajapati-3126a31aa/"
             target="/blank"
