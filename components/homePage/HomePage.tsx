c0bfe1b178dabff21b03ca3c9cf6a25b536793f7
Manishhh24 <190420116054.it19@scet.ac.in>
2023-06-24 14:17:42 +0530

    HomePage.tsx file created

---
+++ components/homePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Introduction from "./Introduction";
+
+const HomePage = () => {
+  return (
+    <>
+      <Introduction />
+    </>
+  );
+};
+
+export default HomePage;
