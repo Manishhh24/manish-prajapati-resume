928dd43816da2a892b8d9aa46877ec4d21035819
Manishhh24 <190420116054.it19@scet.ac.in>
2023-06-27 21:57:03 +0530

    HomeFieldsDisplay component added in HomePage.tsx file

--- components/homePage/HomePage.tsx
+++ components/homePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Introduction from "./Introduction";
+import HomeFieldsDisplay from "./HomeFieldsDisplay";
 
 const HomePage = () => {
   return (
     <>
       <Introduction />
+      <HomeFieldsDisplay />
     </>
   );
 };
