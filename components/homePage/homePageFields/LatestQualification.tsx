8358f45f6c28ac24e36af87a33da684a57d2540c
Manishhh24 <190420116054.it19@scet.ac.in>
2023-06-27 21:56:32 +0530

    LatestQualification file code added

--- components/homePage/homePageFields/LatestQualification.tsx
+++ components/homePage/homePageFields/LatestQualification.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { BsPlusLg } from "react-icons/bs";
+import LatestQualificationContent from "./LatestQualificationContent";
 
 const LatestQualification = () => {
-  return <></>;
+  const [design, setDesign] = useState({ bsPlus: "rotate-0" });
+  return (
+    <>
+      <article className="px-7 bg-blue-100">
+        <div className="pt-10 bg-gradient-to-r from-blue-100 via-white to-blue-100">
+          <LatestQualificationContent />
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default LatestQualification;
