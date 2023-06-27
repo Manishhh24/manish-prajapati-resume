e995515354e956a6a7e4c3d53cf3995024172295
Manishhh24 <190420116054.it19@scet.ac.in>
2023-06-27 22:03:15 +0530

    latestQualification content file created

---
+++ constants/components/homePage/latestQualification.ts
@@ -0,0 +1,1 @@
+export const latestQualification = [{label:"Latest Qualification", content:["Bachelor Of Engineering, Information Technology (2019-23), Sarvajanik College of Engineering and Technology"]}];
