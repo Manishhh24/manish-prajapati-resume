4371af2d474c978a481a34e59223963ba85817da
Manishhh24 <190420116054.it19@scet.ac.in>
2023-07-16 13:09:18 +0530

    space removed from HeaderContent file

--- components/header/HeaderContent.tsx
+++ components/header/HeaderContent.tsx
@@ -4,7 +4,7 @@ import { AiOutlineMail, AiFillLinkedin } from "react-icons/ai";
 import { SiHackerrank } from "react-icons/si";
 
 const HeaderContent = () => {
-  return (  
+  return (
     <>
       <div className="flex justify-between py-5 bg-gradient-to-br from-blue-300 via-blue-400 to-blue-500 max-w-screen-2xl items-center">
         <h1 className="text-sm xxs:text-lg sm:text-2xl md:text-5xl text-white text-left pl-7 font-libreBaskerVille">
