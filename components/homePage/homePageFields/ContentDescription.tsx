fc38dfb07ed7fbc1ef7252a32d04f3e45d0239c5
Manishhh24 <190420116054.it19@scet.ac.in>
2023-07-16 13:10:34 +0530

    Content Description file code added

--- components/homePage/homePageFields/ContentDescription.tsx
+++ components/homePage/homePageFields/ContentDescription.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { SiVbulletin } from "react-icons/si";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
-const ContentDescription = () => {
-  return <div>ContentDescription</div>;
+const ContentDescription = ({
+  description,
+  seeMore,
+}: {
+  description: string[];
+  seeMore: string;
+}) => {
+  const [hover, setHover] = useState("translate-x-0");
+  return (
+    <div>
+      {description.map((content, i) => {
+        return (
+          <span className="flex">
+            <SiVbulletin className="mr-2 min-w-max" />
+            <h4 className="text-[12px] xs:text-[15px] text-gray-800" key={i}>
+              {content}
+            </h4>
+          </span>
+        );
+      })}
+
+      {seeMore !== "" && (
+        <h1
+          onMouseOver={() => setHover("translate-x-2")}
+          onMouseOut={() => setHover("translate-x-0")}
+          className="ml-5 transition-all duration-500 text-xs md:text-sm flex items-center font-bold md:font-extrabold hover:cursor-pointer w-fit hover:text-blue-800"
+        >
+          {seeMore} &nbsp;&nbsp;{" "}
+          <FaLongArrowAltRight
+            className={`transition-all duration-500 ${hover}`}
+          />
+        </h1>
+      )}
+    </div>
+  );
 };
 
 export default ContentDescription;
