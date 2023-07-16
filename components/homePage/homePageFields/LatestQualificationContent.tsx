c24a7bbbe86c755ef67ecf785a40552704ebf3ae
Manishhh24 <190420116054.it19@scet.ac.in>
2023-07-16 13:12:56 +0530

    LatestQualification-Content file refactored

--- components/homePage/homePageFields/LatestQualificationContent.tsx
+++ components/homePage/homePageFields/LatestQualificationContent.tsx
@@ -1,20 +1,39 @@
 import React, { useState } from "react";
-import { BsPlusCircleDotted } from "react-icons/bs";
+import { BsPlusCircleDotted, BsFillArrowRightSquareFill } from "react-icons/bs";
+import ContentDescription from "./ContentDescription";
 
-const LatestQualificationContent = () => {
+type LatestQualificationContentProps = {
+  label: string;
+  content: string[];
+  description?: string[];
+  seeMore?: string;
+};
+
+const LatestQualificationContent: React.FC<LatestQualificationContentProps> = ({
+  label,
+  content,
+  description,
+  seeMore,
+}) => {
   const [design, setDesign] = useState({
     bsPlus: "rotate-0",
     borderWidth: "w-0",
     borderOpacity: "opacity-0",
     click: false,
     hover: false,
+    desDisplay: "hidden",
+    desHeight: "h-0",
+    desBool: false,
   });
+
   return (
     <>
-      <section className="flex flex-col">
+      <section className="flex flex-col pb-5 md:pb-10">
         <div className="flex flex-row">
           <div
-            className="flex flex-row items-center pr-20"
+            className={`flex flex-row items-center xs:pr-20 ${
+              description && "hover:cursor-pointer"
+            }`}
             onMouseEnter={() =>
               setDesign({
                 ...design,
@@ -32,25 +51,21 @@ const LatestQualificationContent = () => {
               })
             }
             onClick={() =>
+              description &&
               setDesign({
                 ...design,
                 click: !design.click,
-                borderWidth: !design.click
-                  ? design.hover
-                    ? "w-full"
-                    : "w-0"
-                  : "w-0",
+                borderWidth: !design.click ? "w-full" : "w-0",
                 bsPlus: design.bsPlus === "rotate-0" ? "rotate-45" : "rotate-0",
-                borderOpacity: !design.click
-                  ? design.hover
-                    ? "opacity-100"
-                    : "opacity-0"
-                  : "opacity-0",
+                borderOpacity: !design.click ? "opacity-100" : "opacity-0",
+                desDisplay: design.desDisplay === "hidden" ? "flex" : "hidden",
+                desHeight: design.desHeight === "h-0" ? "h-fit" : "h-0",
+                desBool: design.desBool ? false : true,
               })
             }
           >
             <BsPlusCircleDotted
-              className={`text-4xl text-blue-700 hover:cursor-pointer ${design.bsPlus} transition-all duration-500`}
+              className={`text-2xl sm:text-3xl md:text-4xl text-blue-700 hover:cursor-pointer ${design.bsPlus} transition-all duration-500`}
               onClick={() =>
                 setDesign(
                   design.bsPlus === "rotate-45"
@@ -59,9 +74,9 @@ const LatestQualificationContent = () => {
                 )
               }
             />
-            <span className="flex flex-col ml-3">
-              <h1 className="text-2xl text-blue-600 font-ysabeauOffice">
-                Latest Qualification
+            <span className="flex flex-col ml-4 md:ml-7">
+              <h1 className="text-md xs:text-xl md:text-2xl text-blue-600 font-ysabeauOffice">
+                {label}
               </h1>
               <span
                 className={`border-[1px] border-blue-800 h-0 ${design.borderWidth} ${design.borderOpacity} transition-all duration-500`}
@@ -69,7 +84,32 @@ const LatestQualificationContent = () => {
             </span>
           </div>
         </div>
-        <div></div>
+        <div
+          className={`ml-10 md:ml-16 ${
+            content.length <= 2
+              ? "flex flex-col"
+              : "grid grid-cols-2 xs:grid-cols-3"
+          }`}
+        >
+          {content.map((item) => (
+            <span className="pt-1 xs:pt-2 flex">
+              <BsFillArrowRightSquareFill className="min-w-max text-blue-700 mt-1 mr-2 text-sm xs:text-lg smd:text-xl" />
+              <p className="text-xs sm:text-sm md:text-lg italic font-robotoCondensed">
+                {item}
+              </p>
+            </span>
+          ))}
+        </div>
+        {description && (
+          <div
+            className={`content-description ml-10 md:ml-16 md:mr-5 mt-4 transition-all duration-1000 ${design.desDisplay} ${design.desHeight}`}
+          >
+            <ContentDescription
+              seeMore={seeMore || ""}
+              description={description}
+            />
+          </div>
+        )}
       </section>
     </>
   );
