921bc01cacd0e5f1e5826102d4d613573fcb33dc
Manishhh24 <190420116054.it19@scet.ac.in>
2023-07-23 12:37:46 +0530

    TechnologyKnown file updated

--- components/technologyKnown/TechnologyKnown.tsx
+++ components/technologyKnown/TechnologyKnown.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import TechTabBar from "./TechTabBar";
 
 const TechnologyKnown = () => {
-  return <></>;
+  return (
+    <>
+      <section className="py-2 bg-gradient-to-l from-blue-100 via-white to-blue-50">
+        <h1 className="w-fit text-blue-600 py-2 text-2xl mx-auto">
+          Technologies Known
+        </h1>
+        <TechTabBar />
+      </section>
+    </>
+  );
 };
 
 export default TechnologyKnown;
