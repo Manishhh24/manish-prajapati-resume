16c9576b48ded69b2a4864d21ed815556eec5498
Manishhh24 <190420116054.it19@scet.ac.in>
2023-07-23 12:35:26 +0530

    TechTabBar file functionality added

--- components/technologyKnown/TechTabBar.tsx
+++ components/technologyKnown/TechTabBar.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { techTabs } from "@/constants/components/technologyKnown/technologies";
 
 const TechTabBar = () => {
-  return <div className="flex"></div>;
+  const [active, setActive] = useState(0);
+  return (
+    <div className="flex my-5 w-fit mx-auto gap-16 border-b border-b-blue-800 pb-4 px-5">
+      {techTabs.map((item, i) => (
+        <span
+          key={i}
+          className={`text-blue-700 hover:cursor-pointer ${
+            active === i && "font-bold"
+          }`}
+          onClick={() => setActive(i)}
+        >
+          {item}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default TechTabBar;
