829d377a7aaf622bf6710f5d0649cf5d80d0537b
Manishhh24 <190420116054.it19@scet.ac.in>
2023-07-23 12:37:09 +0530

    TechnologyKnown component added

--- components/homePage/HomePage.tsx
+++ components/homePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Introduction from "./Introduction";
 import HomeFieldsDisplay from "./HomeFieldsDisplay";
+import TechnologyKnown from "../technologyKnown/TechnologyKnown";
 
 const HomePage = () => {
   return (
     <>
       <Introduction />
       <HomeFieldsDisplay />
+      <TechnologyKnown />
     </>
   );
 };
